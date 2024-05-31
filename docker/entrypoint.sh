7573500fc0d184e3852c4da7d253d1c431f046f1
AbdelilahXR <125505938+AbdelilahXR@users.noreply.github.com>
2024-05-31 17:03:32 +0100

    Update entrypoint.sh - comment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,39 +1,39 @@
-#!/bin/bash
+# #!/bin/bash
 
-if [ ! -f "vendor/autoload.php" ]; then
-    echo "=== Step 1: Running composer install ==="
-    composer install --no-progress --no-interaction
-fi
+# if [ ! -f "vendor/autoload.php" ]; then
+#     echo "=== Step 1: Running composer install ==="
+#     composer install --no-progress --no-interaction
+# fi
 
-if [ ! -f ".env" ]; then
-    echo "Creating env file for env $APP_ENV"
-    cp .env.example .env
-else
-    echo "env file exists."
-fi
+# if [ ! -f ".env" ]; then
+#     echo "Creating env file for env $APP_ENV"
+#     cp .env.example .env
+# else
+#     echo "env file exists."
+# fi
 
-echo "=== Step 2: Installing npm packages and building ==="
-npm install && npm run build
+# echo "=== Step 2: Installing npm packages and building ==="
+# npm install && npm run build
 
 
-echo "=== Step 4: Generating application key ==="
-php artisan key:generate
+# echo "=== Step 4: Generating application key ==="
+# php artisan key:generate
 
-role=${CONTAINER_ROLE:-app}
+# role=${CONTAINER_ROLE:-app}
 
-if [ "$role" = "app" ]; then
-    php artisan migrate
-    php artisan cache:clear
-    php artisan config:clear
-    php artisan route:clear
-    php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
-    exec docker-php-entrypoint "$@"
-elif [ "$role" = "queue" ]; then
-    echo "Running the queue ... "
-    php /var/www/artisan queue:work --verbose --tries=3 --timeout=180
-elif [ "$role" = "websocket" ]; then
-    echo "Running the websocket server ... "
-    php artisan websockets:serve
-fi
+# if [ "$role" = "app" ]; then
+#     php artisan migrate
+#     php artisan cache:clear
+#     php artisan config:clear
+#     php artisan route:clear
+#     php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+#     exec docker-php-entrypoint "$@"
+# elif [ "$role" = "queue" ]; then
+#     echo "Running the queue ... "
+#     php /var/www/artisan queue:work --verbose --tries=3 --timeout=180
+# elif [ "$role" = "websocket" ]; then
+#     echo "Running the websocket server ... "
+#     php artisan websockets:serve
+# fi
 
 
